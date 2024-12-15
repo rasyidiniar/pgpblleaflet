2838798b481edef7c0dc9e35588aa7a04ef2303c
rasyidiniar <rasyidiniar21@gmail.com>
2024-12-16 01:06:06 +0700

    Marker Wisata

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,75 +1,217 @@
-import { Component } from '@angular/core';
-import * as L from 'leaflet'; //cannot find declaration
+// import { Component, OnInit } from '@angular/core';
+// import * as L from 'leaflet';
+// import { HttpClient } from '@angular/common/http';
+
+// @Component({
+//   selector: 'app-home',
+//   templateUrl: 'home.page.html',
+//   styleUrls: ['home.page.scss'],
+// })
+// export class HomePage implements OnInit {
+//   map!: L.Map;
+//   selectedBasemap: string = 'streets'; // Default basemap
+//   tileLayer!: L.TileLayer; // To store the map layer
+//   locations: any[] = []; // Array to store location data
+
+//   constructor(private http: HttpClient) { }
+
+//   ngOnInit() {
+//     // Load location data from JSON file
+//     this.http.get<any[]>('assets/mahasiswa.json').subscribe(data => {
+//       this.locations = data;
+//       this.loadMap();  // Initialize the map after data is loaded
+//     });
+//   }
+
+//   ionViewDidEnter() {
+//     // The map will load after location data is available
+//     if (this.locations.length > 0) {
+//       this.loadMap();
+//     }
+//   }
+
+//   loadMap() {
+//     // Initialize the map with coordinates for Yogyakarta
+//     this.map = L.map('mapId').setView([-7.804117654829307, 110.17924557336778], 11);
+
+//     // Add the initial tile layer
+//     this.addTileLayer(this.selectedBasemap);
+
+//     // Add markers from JSON data
+//     this.locations.forEach(location => {
+//       // Ensure latitude and longitude are valid numbers
+//       const lat = parseFloat(location.Latitude);
+//       const lng = parseFloat(location.Longitude);
+
+//       if (!isNaN(lat) && !isNaN(lng)) {
+//         L.circleMarker([lat, lng], {
+//           radius: 20,  // Circle radius
+//           color: 'red',  // Circle border color
+//           fillColor: 'yellow',  // Circle fill color
+//           fillOpacity: 0.5,  // Circle fill opacity
+//         })
+//           .addTo(this.map)
+//           .bindPopup(`
+//             <b>Nama:</b> ${location.Nama}<br>
+//             <b>Rating:</b> ${location.Rating}<br>
+//             <b>Alamat:</b> ${location.Alamat}<br>
+//             <b>Jam Buka:</b> ${location['Jam Buka']}<br>
+//           `);
+//       } else {
+//         console.error(`Invalid coordinates for location: ${location.Nama}`);
+//       }
+//     });
+//   }
+
+//   // Add a tile layer to the map
+//   addTileLayer(basemap: string) {
+//     // Remove the previous tile layer if it exists
+//     if (this.tileLayer) {
+//       this.tileLayer.remove();
+//     }
+
+//     // Select URL based on the selected basemap
+//     let tileUrl: string;
+//     switch (basemap) {
+//       case 'streets':
+//         tileUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'; // OpenStreetMap streets
+//         break;
+//       case 'topo':
+//         tileUrl = 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'; // OpenTopoMap
+//         break;
+//       case 'satellite':
+//         tileUrl = 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'; // Satellite map from ArcGIS
+//         break;
+//       case 'topo-vector':
+//         tileUrl = 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'; // OpenTopoMap as vector
+//         break;
+//       case 'carto-positron':
+//         tileUrl = 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'; // Minimalist map from CartoDB (Positron)
+//         break;
+//       default:
+//         tileUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'; // Default to OpenStreetMap
+//         break;
+//     }
+
+//     // Add the new tile layer
+//     this.tileLayer = L.tileLayer(tileUrl, {
+//       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//     }).addTo(this.map);
+//   }
+
+//   // Change the basemap
+//   changeBasemap() {
+//     this.addTileLayer(this.selectedBasemap);
+//   }
+// }
+
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
-  //Deklarasi 3 Variabel
+export class HomePage implements OnInit {
   map!: L.Map;
-  markerIcon!: L.Icon;
-  layerControl!: L.Control.Layers;
+  selectedBasemap: string = 'streets'; // Default basemap
+  tileLayer!: L.TileLayer; // To store the map layer
+  locations: any[] = []; // Array to store location data
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
   ngOnInit() {
-
-
+    // Load location data from JSON file
+    this.http.get<any[]>('assets/mahasiswa.json').subscribe(data => {
+      this.locations = data;
+      this.loadMap();  // Initialize the map after data is loaded
+    });
   }
 
-  //Inisialisasi Peta
   ionViewDidEnter() {
-    this.map = L.map('mapId').setView([-7.770397767812406, 110.38205714193435], 13)
+    // The map will load after location data is available
+    if (this.locations.length > 0) {
+      this.loadMap();
+    }
+  }
 
-    //Marker
-    this.markerIcon = L.icon({
-      iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
-      iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
-      shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
-      iconSize: [30, 41],
-      iconAnchor: [15, 40],
+  loadMap() {
+    // Initialize the map with coordinates for Yogyakarta
+    this.map = L.map('mapId').setView([-7.804117654829307, 110.17924557336778], 11);
+
+    // Add the initial tile layer
+    this.addTileLayer(this.selectedBasemap);
+
+    // Define a custom icon for the markers (tourism icon example)
+    const tourismIcon = L.icon({
+      iconUrl: 'assets/icon/marker.png',  // Your icon file path (replace with your own tourism icon image)
+      iconSize: [32, 32],  // Size of the icon
+      iconAnchor: [16, 32],  // Point of the icon that will correspond to marker's location
+      popupAnchor: [0, -32],  // Popup position relative to icon
     });
 
-    const marker = L.marker([-7.770397767812406, 110.38205714193435], {icon: this.markerIcon}).addTo(this.map)
-    .bindPopup('Wisdom Park')
-    .openPopup();
+    // Add markers from JSON data
+    this.locations.forEach(location => {
+      // Ensure latitude and longitude are valid numbers
+      const lat = parseFloat(location.Latitude);
+      const lng = parseFloat(location.Longitude);
 
-    //Basemap
-    var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '© OpenStreetMap'
+      if (!isNaN(lat) && !isNaN(lng)) {
+        // Use the custom tourism icon
+        L.marker([lat, lng], { icon: tourismIcon })
+          .addTo(this.map)
+          .bindPopup(`
+            <b>Nama:</b> ${location.Nama}<br>
+            <b>Rating:</b> ${location.Rating}<br>
+            <b>Alamat:</b> ${location.Alamat}<br>
+            <b>Jam Buka:</b> ${location['Jam Buka']}<br>
+          `);
+      } else {
+        console.error(`Invalid coordinates for location: ${location.Nama}`);
+      }
     });
+  }
 
-    var osmHOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France'
-    });
+  // Add a tile layer to the map
+  addTileLayer(basemap: string) {
+    // Remove the previous tile layer if it exists
+    if (this.tileLayer) {
+      this.tileLayer.remove();
+    }
 
-    var stamen = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-    });
+    // Select URL based on the selected basemap
+    let tileUrl: string;
+    switch (basemap) {
+      case 'streets':
+        tileUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'; // OpenStreetMap streets
+        break;
+      case 'topo':
+        tileUrl = 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'; // OpenTopoMap
+        break;
+      case 'satellite':
+        tileUrl = 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'; // Satellite map from ArcGIS
+        break;
+      case 'topo-vector':
+        tileUrl = 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'; // OpenTopoMap as vector
+        break;
+      case 'carto-positron':
+        tileUrl = 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'; // Minimalist map from CartoDB (Positron)
+        break;
+      default:
+        tileUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'; // Default to OpenStreetMap
+        break;
+    }
 
-    var gmaps = L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-      maxZoom: 19,
-      subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-    });
-
-
-    var baseMaps = {
-    "OpenStreetMap": osm,
-    "OpenStreetMap.HOT": osmHOT,
-    "Stamen Toner" : stamen,
-    "Google Maps" : gmaps,
-    };
-
-    L.control.layers(baseMaps).addTo(this.map);
-
-    //Peta OSM Default
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    // Add the new tile layer
+    this.tileLayer = L.tileLayer(tileUrl, {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.map);
   }
 
+  // Change the basemap
+  changeBasemap() {
+    this.addTileLayer(this.selectedBasemap);
+  }
 }
